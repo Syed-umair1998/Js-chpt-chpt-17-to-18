4b93aa45b58c10ff71a2d614c15e1f98f924868b
Syed-umair1998 <160543227+Syed-umair1998@users.noreply.github.com>
2024-03-23 04:16:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// question #1
+// var multidimensional_array = [
+//     [],/*for rows*/
+//     [],
+// ];
+
+// question #2
+// var multidimensional_array2 = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// for(var a= 0; a< 3; a++){
+// document.write(multidimensional_array2[a] + "<br>");
+// }
+
+// question #3
+// for (var i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// question #4
+// var tableNuumber = prompt("enter table number")
+// var tablelength = prompt("enter table length")
+
+// for (var i = 1; i <= tablelength; i++){
+//     document.write(tableNuumber + " x "  + i + " = " + tableNuumber * i + "<br/>")
+// }
+
+// question #5
+// var fruitsarr = ["apple","banana","mango","orange","strawberry"]
+// console.log(fruitsarr[4]);
+
+// question #6
+// Part-A 
+// for (var i = 1; i<= 15; i++){
+//     document.write(i + "<br/>")
+// }
+
+// Part-B
+// for (var i = 10; i>= 1; i--){
+//     document.write(i + "<br/>")
+// }
+
+// PArt-C
+// for(let i=0; i<=20; i+=2){
+//     document.write(i + "<br/>")
+// }
+
+// Part-D
+// for(let i=2000; i<=20000; i+=2000){
+//         document.write(i + "<br/>")
+// }
